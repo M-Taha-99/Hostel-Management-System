d1e0638b4611903d123c7ad121f00ff9fc26878e
Muhammad Taha <h.m.taha7198@gmail.com>
2025-01-06 07:36:57 +0500

    database commit

--- DATABASE/hostel.sql
+++ DATABASE/hostel.sql
@@ -42,7 +42,7 @@ CREATE TABLE `admin` (
 --
 
 INSERT INTO `admin` (`id`, `username`, `email`, `password`, `reg_date`, `updation_date`) VALUES
-(1, 'admin', 'anuj.lpu1@gmail.com', 'Test@1234', '2016-04-04 20:31:45', '2016-04-17');
+(1, 'Taha', 'h.m.taha7198@gmail.com', 'Taha1234', '2016-04-04 20:31:45', '2016-04-17');
 
 -- --------------------------------------------------------
 
@@ -76,13 +76,10 @@ CREATE TABLE `courses` (
 --
 
 INSERT INTO `courses` (`id`, `course_code`, `course_sn`, `course_fn`, `posting_date`) VALUES
-(1, 'B10992', 'B.Tech', 'Bachelor  of Technology', '2020-07-04 19:31:42'),
-(2, 'BCOM1453', 'B.Com', 'Bachelor Of commerce ', '2020-07-04 19:31:42'),
-(3, 'BSC12', 'BSC', 'Bachelor  of Science', '2020-07-04 19:31:42'),
-(4, 'BC36356', 'BCA', 'Bachelor Of Computer Application', '2020-07-04 19:31:42'),
-(5, 'MCA565', 'MCA', 'Master of Computer Application', '2020-07-04 19:31:42'),
-(6, 'MBA75', 'MBA', 'Master of Business Administration', '2020-07-04 19:31:42'),
-(7, 'BE765', 'BE', 'Bachelor of Engineering', '2020-07-04 19:31:42');
+(1, 'BIT8855', 'BSIT', 'Bachelora in Information Technology', '2020-07-04 19:31:42'),
+(2, 'BCS7788', 'BSCS', 'Bachelors in Computer Science ', '2020-07-04 19:31:42'),
+(3, 'BDS2211', 'BSDS', 'Bachelor  in Data Science', '2020-07-04 19:31:42'),
+(4, 'BSE4856', 'BSSE', 'Bachelors in Software Engineering', '2020-07-04 19:31:42');
 
 -- --------------------------------------------------------
 
@@ -127,7 +124,7 @@ CREATE TABLE `registration` (
 --
 
 INSERT INTO `registration` (`id`, `roomno`, `seater`, `feespm`, `foodstatus`, `stayfrom`, `duration`, `course`, `regno`, `firstName`, `middleName`, `lastName`, `gender`, `contactno`, `emailid`, `egycontactno`, `guardianName`, `guardianRelation`, `guardianContactno`, `corresAddress`, `corresCIty`, `corresState`, `corresPincode`, `pmntAddress`, `pmntCity`, `pmnatetState`, `pmntPincode`, `postingDate`, `updationDate`) VALUES
-(2, 100, 5, 8000, 1, '2020-08-01', 6, 'Bachelor  of Technology', 10806121, 'Anuj', '', 'kumar', 'male', 1234567890, 'ak@gmail.com', 1236547890, 'ABC', 'XYZ', 98756320000, 'ABC 12345 XYZ Street', 'New Delhi', 'Delhi (NCT)', 110001, 'ABC 12345 XYZ Street', 'New Delhi', 'Delhi (NCT)', 110001, '2020-07-20 14:58:26', NULL);
+(2, 100, 5, 8000, 1, '2020-08-01', 6, 'Bachelors in Information Technology', 10806121, 'Hassan', '', 'Ahmad', 'male', 1234567890, 'Hassan@gmail.com', 1236547890, 'ABC', 'XYZ', 98756320000, 'ABC 12345 XYZ Street', 'Lahore', 'Punjab', 110001, 'ABC 12345 XYZ Street', 'Lahore', 'Punjab', 110001, '2020-07-20 14:58:26', NULL);
 
 -- --------------------------------------------------------
 
@@ -170,42 +167,12 @@ CREATE TABLE `states` (
 --
 
 INSERT INTO `states` (`id`, `State`) VALUES
-(1, 'Andaman and Nicobar Island (UT)'),
-(2, 'Andhra Pradesh'),
-(3, 'Arunachal Pradesh'),
-(4, 'Assam'),
-(5, 'Bihar'),
-(6, 'Chandigarh (UT)'),
-(7, 'Chhattisgarh'),
-(8, 'Dadra and Nagar Haveli (UT)'),
-(9, 'Daman and Diu (UT)'),
-(10, 'Delhi (NCT)'),
-(11, 'Goa'),
-(12, 'Gujarat'),
-(13, 'Haryana'),
-(14, 'Himachal Pradesh'),
-(15, 'Jammu and Kashmir'),
-(16, 'Jharkhand'),
-(17, 'Karnataka'),
-(18, 'Kerala'),
-(19, 'Lakshadweep (UT)'),
-(20, 'Madhya Pradesh'),
-(21, 'Maharashtra'),
-(22, 'Manipur'),
-(23, 'Meghalaya'),
-(24, 'Mizoram'),
-(25, 'Nagaland'),
-(26, 'Odisha'),
-(27, 'Puducherry (UT)'),
-(28, 'Punjab'),
-(29, 'Rajastha'),
-(30, 'Sikkim'),
-(31, 'Tamil Nadu'),
-(32, 'Telangana'),
-(33, 'Tripura'),
-(34, 'Uttarakhand'),
-(35, 'Uttar Pradesh'),
-(36, 'West Bengal');
+(1, 'Punjab'),
+(2, 'Sindh'),
+(3, 'Khyber Pakhtunkhawa'),
+(4, 'Baluchistan'),
+(5, 'Azad Jammu & Kashmir'),
+(6, 'Federal (capital)');
 
 -- --------------------------------------------------------
 
@@ -256,7 +223,7 @@ CREATE TABLE `userregistration` (
 --
 
 INSERT INTO `userregistration` (`id`, `regNo`, `firstName`, `middleName`, `lastName`, `gender`, `contactNo`, `email`, `password`, `regDate`, `updationDate`, `passUdateDate`) VALUES
-(3, '10806121', 'Anuj', '', 'kumar', 'male', 1234567890, 'test@gmail.com', 'Test@123', '2020-07-20 14:56:18', NULL, NULL);
+(3, '10806121', 'Hassan', '', 'Ahmad', 'male', 1234567890, 'Hassan@gmail.com', 'Test@123', '2020-07-20 14:56:18', NULL, NULL);
 
 --
 -- Indexes for dumped tables
